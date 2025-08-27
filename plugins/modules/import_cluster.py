f90cc6ee139ad5279286aa492cd2c2e0710af87c
Alberto Gonzalez <josegonz@redhat.com>
2025-08-27 21:46:21 +0200

    Adding import_cluster.py and switch to version v0.0.4

---
+++ plugins/modules/import_cluster.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2023, Alberto Gonzalez <alberto.gonzalez@redhat.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+import requests
+import json
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.rhpds.assisted_installer.plugins.module_utils import access_token
+
+
+DOCUMENTATION = r'''
+---
+module: import_cluster
+
+short_description: Import an AI cluster using minimal data associated with existing OCP cluster, in order to allow adding day2 hosts to that cluster
+
+version_added: "1.0.0"
+
+description: Import an AI cluster using minimal data associated with existing OCP cluster, in order to allow adding day2 hosts to that cluster
+
+options:
+    cluster_name:
+        description: All hosts that register will be associated with the specified cluster.
+        required: true
+        type: str
+    offline_token:
+        description: Offline token from console.redhat.com
+        required: true
+    api_vip_dnsname:
+        description: API DNS Domain of the cluster to be imported
+        required: true
+    openshift_cluster_id:
+        description: Cluster ID of the cluster to be imported
+        required: true
+author:
+    - Alberto Gonzalez (@agonzalezrh)
+'''  # noqa
+
+EXAMPLES = r'''
+- name: Import existing cluster
+  rhpds.assisted_installer.import_cluster:
+    cluster_name: "{{ cluster_name }}"
+    offline_token: "{{ offline_token }}"
+    api_vip_dnsname: "api.domain.com"
+    openshift_cluster_id: "1234"
+  register: imported_cluster
+'''
+RETURN = r'''
+result:
+    description: Result from the API call
+    type: dict
+    returned: always
+'''
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        cluster_name=dict(type='str', required=True),
+        offline_token=dict(type='str', required=True),
+        api_vip_dnsname=dict(type='str', required=True),
+        openshift_cluster_id=dict(type='str', required=True),
+    )
+
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=5)
+    session.mount('https://', adapter)
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    response = access_token._get_access_token(module.params['offline_token'])
+    if response.status_code != 200:
+        module.fail_json(msg='Error getting access token ', **response.json())
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    result['access_token'] = response.json()["access_token"]
+
+    headers = {
+        "Authorization": "Bearer " + response.json()["access_token"],
+        "Content-Type": "application/json"
+    }
+    params = module.params.copy()
+    params.pop("cluster_name")
+    params.pop("offline_token")
+    params["name"] = module.params["cluster_name"]
+    response = session.post(
+        "https://api.openshift.com/api/assisted-install/v2/clusters/import",
+        headers=headers,
+        json=params
+    )
+
+    result['result'] = response.json()
+
+    if "code" in response.json():
+        module.fail_json(msg='Request failed: ', **result)
+    else:
+        result['changed'] = True
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
